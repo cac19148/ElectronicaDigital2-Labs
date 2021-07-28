49efcd213d0bdd19871b21e0ab27000dfb2e1f2b
cac19148 <68085111+cac19148@users.noreply.github.com>
2021-07-28 14:43:56 -0600

    Create LibreriaConfig.p1.d

---
+++ Lab2/ElectronicaDigital2_Lab2.X/build/default/production/_ext/381910011/LibreriaConfig.p1.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/381910011/LibreriaConfig.p1:  \
+../../OneDrive/Escritorio/lab\ fredy/Lab_02.X/LibreriaConfig.c  \
+../../OneDrive/Escritorio/lab\ fredy/Lab_02.X/Libreria.h  \
+../../OneDrive/Escritorio/lab\ fredy/Lab_02.X/LCD.h 
