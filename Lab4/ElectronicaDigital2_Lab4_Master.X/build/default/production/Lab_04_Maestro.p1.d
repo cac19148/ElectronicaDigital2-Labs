ce72b33b3288e56da9926eca1bb5eaf743734a5c
cac19148 <68085111+cac19148@users.noreply.github.com>
2021-08-12 11:23:16 -0600

    Create Lab_04_Maestro.p1.d

---
+++ Lab4/ElectronicaDigital2_Lab4_Master.X/build/default/production/Lab_04_Maestro.p1.d
@@ -0,0 +1,5 @@
+build/default/production/Lab_04_Maestro.p1:  \
+Lab_04_Maestro.c  \
+../../../ferna/MPLABXProjects/LIB.X/LIB.h  \
+../../../ferna/MPLABXProjects/LIB.X/I2C.h  \
+../../../ferna/MPLABXProjects/LIB.X/LCD.h 
